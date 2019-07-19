c8756ef656810f9126ef31ffc74358b8f2e32f0f
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-07-19 15:39:56 -0700

    deleted node module from main repo directory

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,15 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { UncontrolledCollapse, Button, CardBody } from "reactstrap";
 
 function StarWarsCard(props) {
   console.log(props);
   return (
     <Card>
-      <h2>Name: {props.character.name}</h2>
-      <div>Height: {props.character.height}</div>
-      <div>Mass: {props.character.mass}</div>
-      <div>Birth Year: {props.character.birth_year}</div>
-      <div>Eye Color: {props.character.eye_color}</div>
+      <Button color="primary" id="toggler" style={{ marginBottom: "1rem" }}>
+        <h2> Name: {props.character.name}</h2>
+      </Button>
+      <UncontrolledCollapse toggler="#toggler">
+        <CardBody>
+          <div>Height: {props.character.height}</div>
+          <div>Mass: {props.character.mass}</div>
+          <div>Birth Year: {props.character.birth_year}</div>
+          <div>Eye Color: {props.character.eye_color}</div>
+        </CardBody>
+      </UncontrolledCollapse>
     </Card>
   );
 }
@@ -22,4 +29,7 @@ const Card = styled.div`
   color: darkblue;
   margin-bottom: 20px;
   padding: 20px;
+  :hover {
+    color: red;
+  }
 `;
